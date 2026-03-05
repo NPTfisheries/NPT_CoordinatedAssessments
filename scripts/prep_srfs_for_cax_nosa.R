ccb4ec45e6f9ba3f3c733b0eb4198f3d6777ca4a
Mike Ackerman <dirtpathwanderer@gmail.com>
2026-03-05 14:55:22 -0700

    added some weir ests plus progress b4 round of QC

--- scripts/prep_srfs_for_cax_nosa.R
+++ scripts/prep_srfs_for_cax_nosa.R
@@ -5,7 +5,7 @@
 #   IPTDS-based escapement estimates versus expanded spawner abundance estimates (accounting for unmonitored habitat). 
 # 
 # Created Date: January 23, 2026
-#   Last Modified: February 18, 2026
+#   Last Modified: March 5, 2026
 #
 # Notes:
 
@@ -105,7 +105,7 @@ sr_pop_df = pop_df %>%
 # prep age_p_df to join to abundance results and formatted for CAX
 age_p_for_cax = age_p_df %>%
   select(species:upper95ci) %>%
-  mutate(across(c(median, lower95ci, upper95ci), ~ signif(round(.x, 8), 8)),
+  mutate(across(c(median, lower95ci, upper95ci), ~ round(.x, 8)),
          param = str_remove(param, "^p_"),
          param = str_replace(param, "age_", "Age")) %>%
   #group_by(species, spawn_yr, popid) %>%
@@ -161,34 +161,48 @@ srfs_to_cax = pop_esc_df %>%
   # toss out Tucannon estimates & estimates for multiple populations
   filter(!str_detect(CommonPopName, "/"),                            ### we could submit these as PopFit = Multiple, which would require additional work
          !str_detect(CommonPopName, "SNTUC")) %>%
-  # do we want to try to add estimates from RAPH and PAHH? if so, these would need to be QC'd
-  # bind_rows(
-  #   site_esc_df %>%
-  #     filter(site %in% c("PAHH", "RAPH") & !(species == "Steelhead" & site == "RAPH")) %>%
-  #     select(-site_operational) %>%
-  #     rename(pop_sites = site, CommonName = species) %>%
-  #     mutate(mpg = case_when(
-  #       CommonName == "Chinook"   & pop_sites == "RAPH" ~ "South Fork Salmon River",
-  #       CommonName == "Chinook"   & pop_sites == "PAHH" ~ "Upper Salmon River",
-  #       CommonName == "Steelhead" & pop_sites == "PAHH" ~ "Salmon River"
-  #     )) %>%
-  #     mutate(CommonPopName = case_when(
-  #       CommonName == "Chinook"   & pop_sites == "RAPH" ~ "SRLSR",
-  #       CommonName == "Chinook"   & pop_sites == "PAHH" ~ "SRPAH",
-  #       CommonName == "Steelhead" & pop_sites == "PAHH" ~ "SRPAH-s"
-  #     )) %>%
-  #     mutate(p_qrf = case_when(
-  #       CommonName == "Chinook"   & pop_sites == "RAPH" ~ 0.26,
-  #       CommonName == "Chinook"   & pop_sites == "PAHH" ~ 0.32,
-  #       CommonName == "Steelhead" & pop_sites == "PAHH" ~ 0.99
-  #     ))
-  # ) %>%
+  # add estimates from RAPH, PAHH, and SALEFT. These will only be unexpanded
+  bind_rows(
+    site_esc_df %>%
+      filter(site %in% c("PAHH", "RAPH", "SALEFT")) %>%
+      transmute(
+        pop_sites = site,
+        CommonName = species,
+        SpawningYear = spawn_yr,
+        median, lower95ci, upper95ci,
+        notes,
+        no_expand = TRUE
+      )
+  ) %>%
+  mutate(
+    no_expand = coalesce(no_expand, FALSE),
+    CommonPopName = case_when(
+      CommonName == "Chinook"   & pop_sites == "PAHH"   ~ "SRPAH",
+      CommonName == "Chinook"   & pop_sites == "RAPH"   ~ "SRLSR",
+      CommonName == "Chinook"   & pop_sites == "SALEFT" ~ "SREFS",
+      CommonName == "Steelhead" & pop_sites == "PAHH"   ~ "SRPAH-s",
+      CommonName == "Steelhead" & pop_sites == "RAPH"   ~ "SRLSR-s",
+      CommonName == "Steelhead" & pop_sites == "SALEFT" ~ "SREFS-s",
+      TRUE ~ CommonPopName
+    ),
+    p_qrf = case_when(
+      CommonName == "Chinook"   & pop_sites == "PAHH"   ~ 0.9743887,
+      CommonName == "Chinook"   & pop_sites == "RAPH"   ~ 0.0000000,
+      CommonName == "Chinook"   & pop_sites == "SALEFT" ~ 0.5085837,
+      CommonName == "Steelhead" & pop_sites == "PAHH"   ~ 0.9774971,
+      CommonName == "Steelhead" & pop_sites == "RAPH"   ~ 0.1277831,
+      CommonName == "Steelhead" & pop_sites == "SALEFT" ~ 0.4114101,
+      TRUE ~ p_qrf
+    ),
+    Comments = coalesce(Comments, notes)
+  ) %>%
+  select(-notes) %>%
   # attach population metadata from sr_pop_df
   mutate(CommonName = recode(CommonName, "Chinook" = "Chinook Salmon")) %>%
   left_join(sr_pop_df, by = c("CommonName", "CommonPopName")) %>%
   # add lat/lon based on the first site in pop_sites
   mutate(site_code = str_extract(pop_sites, "^[^,]+")) %>%
-  left_join(site_ll %>% st_drop_geometry(), by = "site_code") %>%
+  left_join(site_ll, by = "site_code") %>%
   select(-site_code) %>%
   # set PopFit for median, lower95ci, and upper95ci based on proportion of habitat monitored; expanded ests will be added below
   mutate(PopFit = if_else(p_qrf >= threshhold, "Same", "Portion"),
@@ -198,7 +212,7 @@ srfs_to_cax = pop_esc_df %>%
     bind_rows(
       df,
       df %>%
-        filter(p_qrf < threshhold & PopFit == "Portion") %>%
+        filter(p_qrf < threshhold, PopFit == "Portion", !no_expand) %>%
         mutate(median       = median_exp,
                lower95ci    = lower95ci_exp,
                upper95ci    = upper95ci_exp,
@@ -207,7 +221,7 @@ srfs_to_cax = pop_esc_df %>%
     )
   } %>%
   # expanded ests no longer needed
-  select(-contains("_exp")) %>%
+  select(-contains("_exp"), -no_expand) %>%
   # assign MethodNumber
   mutate(MethodNumber = case_when(
     MetaComments == "STADEM and DABOM"       ~ 2,
@@ -251,7 +265,8 @@ srfs_to_cax = pop_esc_df %>%
                      MetaComments = metacomments,
                      ID = id,
                      contactemail),
-            by = c("CommonName", "CommonPopName", "SpawningYear", "MetaComments")) %>%
+            by = c("CommonName", "CommonPopName", "SpawningYear", "MetaComments"),
+            relationship = "many-to-many") %>%
   filter(MetaComments %in% c("STADEM and DABOM", "STADEM, DABOM, and QRF")) %>%
   # provide my call for add, update, or delete
   mutate(StatusMA = case_when(
@@ -299,7 +314,6 @@ srfs_to_cax = pop_esc_df %>%
          MeasureLocation    = "npt-cdms.nezperce.org",
          OtherDataSources   = "IDFG, ODFW, WDFW, Biomark, QCI, SBT, CTUIR",
          Publish            = "Yes") %>%
-  arrange(CommonName, CommonPopName, SpawningYear) %>%
   # apply TimeSeriesIDs: apply existing ones for unexpanded ests, apply new ones for expanded ests
   left_join(
     ts_tbl, by = c("CommonName", "CommonPopName")
@@ -327,21 +341,12 @@ srfs_to_cax = pop_esc_df %>%
       select(-TimeSeriesID_new)
   } %>%
   # assign CompilerRecordID using TimeSeriesID and SpawningYear
-  mutate(CompilerRecordID = if_else(StatusMA != "DELETE", paste0(TimeSeriesID, "-", SpawningYear), ""))
-
-#----------------------------------------
-# reorder and QC columns to follow CAX data exchange standards
-source("R/nosa_des_spec.R")
-
-# re-order & add missing columns
-srfs_to_cax_ord = apply_cax_des_col_order(srfs_to_cax, nosa_des_spec)
-
-# QC column types
-qc_report = qc_against_des_spec(srfs_to_cax_ord, nosa_des_spec)
+  mutate(CompilerRecordID = if_else(StatusMA != "DELETE", paste0(TimeSeriesID, "-", SpawningYear), "")) %>%
+  arrange(CommonName, CommonPopName, SpawningYear)
 
 #----------------------------------------
 # some final modifications based on QA/QC
-srfs_to_cax_qc = srfs_to_cax_ord %>%
+srfs_to_cax_qc = srfs_to_cax %>%
   select(StatusMA, everything()) %>%
   # in any case where the "Portion" estimate is 0, I don't want to report the expanded estimate.
   group_by(CommonName, CommonPopName, SpawningYear) %>%
@@ -362,6 +367,16 @@ srfs_to_cax_qc = srfs_to_cax_ord %>%
     )
   )
 
+#----------------------------------------
+# reorder and QC columns to follow CAX data exchange standards
+source("R/nosa_des_spec.R")
+
+# re-order & add missing columns
+srfs_to_cax_final = apply_cax_des_col_order(srfs_to_cax_qc, nosa_des_spec)
+
+# QC column types
+qc_report = qc_against_des_spec(srfs_to_cax_final, nosa_des_spec)
+
 # NOTE: Need to remove pop_sites, maybe among other columns at the end?, before final export
 
 # write to excel, if needed
@@ -369,40 +384,36 @@ write_xlsx(srfs_to_cax_qc, path = paste0("output/SnakeRiverFishStatus_Results_4_
 
 #----------------------------------------------------------------------
 # compare prepped SRFS results to existing CAX records submitted by NPT
-comp_df = srfs_to_cax %>%
-  select(CommonName,
-         CommonPopName,
-         SpawningYear,
-         MetaComments,
-         NOSAIJ) %>%
-  rename(NOSAIJ_SRFS = NOSAIJ) %>%
-  full_join(
-    npt_cax_nosa %>%
-      select(CommonName = commonname,
-             CommonPopName = commonpopname,
-             SpawningYear = spawningyear,
-             MetaComments = metacomments,
-             ID = id,
-             NOSAIJ = nosaij) %>%
-      rename(NOSAIJ_CAX = NOSAIJ),
-    by = c("CommonName", "CommonPopName", "SpawningYear", "MetaComments")
-  ) %>%
-  mutate(
-    source = case_when(
-      !is.na(NOSAIJ_SRFS) & !is.na(NOSAIJ_CAX) ~ "BOTH",
-      !is.na(NOSAIJ_SRFS) &  is.na(NOSAIJ_CAX) ~ "SRFS_ONLY",
-       is.na(NOSAIJ_SRFS) & !is.na(NOSAIJ_CAX) ~ "CAX_ONLY",
-      TRUE ~ NA_character_
-    )
-  )
-
-# tentative records to delete in CAX; these records exist in CAX but I no longer report from SRFS
-records_to_delete = comp_df %>%
-  filter(MetaComments == "STADEM and DABOM", 
-         source       == "CAX_ONLY")
+# comp_df = srfs_to_cax %>%
+#   select(CommonName,
+#          CommonPopName,
+#          SpawningYear,
+#          MetaComments,
+#          NOSAIJ) %>%
+#   rename(NOSAIJ_SRFS = NOSAIJ) %>%
+#   full_join(
+#     npt_cax_nosa %>%
+#       select(CommonName = commonname,
+#              CommonPopName = commonpopname,
+#              SpawningYear = spawningyear,
+#              MetaComments = metacomments,
+#              ID = id,
+#              NOSAIJ = nosaij) %>%
+#       rename(NOSAIJ_CAX = NOSAIJ),
+#     by = c("CommonName", "CommonPopName", "SpawningYear", "MetaComments")
+#   ) %>%
+#   mutate(
+#     source = case_when(
+#       !is.na(NOSAIJ_SRFS) & !is.na(NOSAIJ_CAX) ~ "BOTH",
+#       !is.na(NOSAIJ_SRFS) &  is.na(NOSAIJ_CAX) ~ "SRFS_ONLY",
+#        is.na(NOSAIJ_SRFS) & !is.na(NOSAIJ_CAX) ~ "CAX_ONLY",
+#       TRUE ~ NA_character_
+#     )
+#   )
+# 
+# # tentative records to delete in CAX; these records exist in CAX but I no longer report from SRFS
+# records_to_delete = comp_df %>%
+#   filter(MetaComments == "STADEM and DABOM", 
+#          source       == "CAX_ONLY")
 
 ### END SCRIPT
-
-
-
-
